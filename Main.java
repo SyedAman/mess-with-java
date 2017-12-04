f1890e7ddb9b951f02d28b403adde1c2e448ec22
Syed Aman <sna274@nyu.edu>
2017-12-04 06:10:27 -0600

    update comments

--- Main.java
+++ Main.java
@@ -1,4 +1,9 @@
 public class Main {
+  /**
+   * Entry point.
+   * @method main
+   * @param  String[] args - A string that could be passed.
+   */
   public static void main(String[] args) {
     System.out.println("hello worlds!");
   }
